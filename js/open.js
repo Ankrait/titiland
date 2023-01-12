cf94512fe3ec92f42ede99687a13b5c5b8ab1d79
Азат Файзрахманов <azat_fayzrahmanov@mail.ru>
2023-01-12 17:39:32 +0300

    fixes

--- js/open.js
+++ js/open.js
@@ -3,10 +3,7 @@ function openFunc(btn, block) {
         block.classList.toggle("dn");
     })
 
-    // let close = block.querySelector(".closebtn");
-    // close.addEventListener("click", () => {
-    //     block.classList.add("dn");
-    // })
+    debugger;
 
     document.addEventListener('click', e => {
         let target = e.target;
@@ -27,7 +24,7 @@ let block2 = document.querySelector(".collection");
 let btn3 = document.querySelector(".btn.nft");
 let block3 = document.querySelector(".nft-card");
 let btn4 = document.querySelector(".btn.sale");
-let block4 = document.querySelector(".nft-card.sale");
+let block4 = document.querySelector("nft-card");
 let btn5 = document.querySelector(".btn.bought");
 let block5 = document.querySelector(".nft-card.bought");
 let btn6 = document.querySelector(".btn.my");
@@ -44,3 +41,28 @@ openFunc(btn5, block5);
 openFunc(btn6, block6);
 openFunc(btn7, block7);
 openFunc(btn8, block8);
+
+// let close1 = block2.querySelector(".collection .closebtn");
+// close1.addEventListener("click", () => {
+//     block1.classList.add("dn");
+// })
+// let close2 = block3.querySelector(".nft-card .closebtn");
+// close2.addEventListener("click", () => {
+//     block2.classList.add("dn");
+// })
+// let close3 = block4.querySelector(".closebtn");
+// close3.addEventListener("click", () => {
+//     block3.classList.add("dn");
+// })
+// let close4 = block5.querySelector("nft-card .closebtn");
+// close4.addEventListener("click", () => {
+//     block4.classList.add("dn");
+// })
+// let close5 = block6.querySelector(".closebtn");
+// close5.addEventListener("click", () => {
+//     block5.classList.add("dn");
+// })
+// let close6 = block7.querySelector(".closebtn");
+// close6.addEventListener("click", () => {
+//     block6.classList.add("dn");
+// })
